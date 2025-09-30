0d1f8a9304c9c8fbb5454f7fb3106c0bbc180e21
Roy Surjano <pxrsurja@cibertec.edu.pe>
2025-09-30 12:15:50 -0500

    refactor: mejorado la estructura de archivos

--- sesion01/01.block-scope.js
+++ sesion01/01.block-scope.js
@@ -43,19 +43,3 @@ try {
 // aqui puedo invocar la funcion
 console.log("la suma([1, 2]) deberia ser 1 + 2 = 3");
 console.log(suma([1, 2])) // resultado seria [1,2]
-
-// CONSTANTES
-// en tiempo de ejecucion NO CAMBIAN
-const PI = 3.1416
-const URL_SERVIDOR = 'http://mi-servidor.com/api/users'
-
-// variables
-
-var myValue = true; // esto YA NO SE USA
-// - no respeta muchas veces el block-scope
-// - no permite guardar valores persistentes (const)
-// - produce muchos problemas de debugging(depuracion)
-// - ya la gran mayoria de navegadores ya soporta LET/CONST
-
-
-const PI = Math.PI
