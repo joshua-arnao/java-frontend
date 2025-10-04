c43532839cb7c6108cfefb2bb4e9c17c245db40b
Roy Surjano <pxrsurja@cibertec.edu.pe>
2025-10-03 22:30:44 -0500

    adding promises example

--- apps/es6/src/promises.ts
+++ apps/es6/src/promises.ts
@@ -52,8 +52,50 @@ new Promise((resolve, reject) => {
   }
 });
 
+export function promiseExample03() {
+  console.log("Visualizando el estado de un promise");
+  // ESTADOS: resolve, reject
+
+  const basicPromise = new Promise((resolve, reject) => {
+    console.log("[ejecucion] estado: PENDING");
+
+    // simulamos con 50% de probabilidad de resolve y reject
+    setTimeout(() => {
+
+      // const exito = Math.random() > 0.5; // 50% de probabilidad true / false
+      const exito = false;
+      console.log('exito -> ', exito)
+
+      if (exito) {
+        console.log("[ejecucion] llamado resolve -> FULFILLED");
+        resolve("Operacion exitosa")
+      } else {
+        console.log("[ejecucion] llamado reject -> REJECTED");
+        reject(new Error("Operacion fallo"));
+      }
+    }, 1000)
+  });
+
+  basicPromise
+    .then((resultado) => {
+      // este callback se ejecuta si resolve() fue llamado
+      console.log("THEN ejecutado, fulfilled", resultado);
+    })
+    .catch((error) => {
+      // este callback se ejecuta cuando reject() es llamado
+      console.log("[CATCH] es llamado, rejected", error);
+    })
+    // hay casos que queremos ejecutar una logica SI O SI
+    .finally(() => {
+      // Siempre se va a ejecutar indistintamente sea fulfilled o rejected
+      console.log("[FINALLY] promise finalizado")
+    })
+
+}
 
 export function promiseExamples() {
   promisesExample01();
   promiseExample02();
+  promiseExample03();
 }
+//
--- sesiones/sesion03/README.md
+++ sesiones/sesion03/README.md
@@ -37,6 +37,49 @@ Una promesa(promise) es un objeto que REPRESENTA el resultado de una operación 
 ![alt text](image-1.png)
 ![alt text](image-3.png)
 
-
-
 ## API Rest json-server
+
+JSON Server es una herramienta que te permite crear una **API REST completa en poco tiempo** sin escribir código del servidor. Es perfecta para estos escenarios
+
+- Prototipos rápidos
+- Testing de aplicaciones
+- Aprendizaje de API REST
+- Demos y presentaciones
+
+ imagina que necesitas una tienda de verdad para probar tu sistema de pagos JSON server es como una **tienda de cartón** completamente funcional no es real pero puedes hacer todas las operaciones como si lo fuera.
+
+![alt text](image-4.png)
+
+### Estructura del proyecto
+
+```plain
+.
+└── api/
+    ├── data/
+    │   └── db.json (aquí declaras tu Base de datos)
+    ├── middlewares/
+    │   ├── logger.js
+    │   └── validator.js
+    ├── server.js
+    ├── package.json
+    └── README.md
+```
+
+### Instalacion
+
+```bash
+# instala json-server
+
+npm install --global json-server
+#  o en forma local
+npm install --save-dev json-server
+```
+
+### json-server en forma global
+
+```bash
+npm install --global json-server
+
+# y lo ejecutamos
+json-server apps/api/data/db.json --port 4001 
+```
