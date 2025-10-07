7b7fc0bf56913054a6b1a6f06bd7b5d86ce66fbe
Roy Surjano <pxrsurja@cibertec.edu.pe>
2025-10-07 17:09:16 -0500

    refactor: ts app and angular app into different folders

--- angular-first/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [{
-  path: "", // ruta principal
-  loadComponent: () => import('./first-component/first-component').then(c => c.FirstComponent)
-}];
--- angular/angular-demo/src/app/app.ts
+++ angular/angular-demo/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('angular-first v2');
+  protected readonly title = signal('angular-demo');
 }
