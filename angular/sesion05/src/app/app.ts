39fd634993502ad6eea7b42f82c2e31419ab4e10
Roy Surjano <pxrsurja@cibertec.edu.pe>
2025-10-13 20:27:03 -0500

    added ejemplo de boton eliinar]

--- angular/sesion05/src/app/app.ts
+++ angular/sesion05/src/app/app.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import { User, UserCard } from './user-card';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  // aqui importo los componentes
+  imports: [NxWelcome, RouterModule, UserCard],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'sesion05';
+
+  mainUser: User | null = { name: "Miguel", email: "carlosz@gmail.com", avatarUrl: "https://static.vecteezy.com/system/resources/previews/009/397/835/non_2x/man-avatar-clipart-illustration-free-png.png" };
+
+  onUserDeleted(event: Event) {
+    console.log("event", event);
+
+    this.mainUser = null;
+    console.log("usuario eliminado");
+  }
+
 }
---
+++ angular/sesion05/src/app/user-card.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+export interface User {
+  // id name, email avatarUrl
+  id?: number;
+  name: string;
+  email: string;
+  avatarUrl?: string;
+}
+
+@Component({
+  selector: 'app-user-card',
+  imports: [CommonModule],
+  templateUrl: './user-card.html',
+  styleUrl: './user-card.css',
+})
+export class UserCard {
+  // puede contener el contrato/interfaz User o un valor nulo
+  @Input() user: User | null = null;
+
+  // este atributo va a contener un emisor de eventos
+  @Output() deleteRequest = new EventEmitter<any>();
+
+  onDeleteClick() {
+    // const isTrue = true;
+    // const name = "William Wallace"
+    console.log("clickeado en el boton eliminar")
+    // aqui tambien puedo emitir valores complejos, como objetos, arreglos
+    this.deleteRequest.emit(10);
+  }
+
+}
