37b24f2d7597a4b637897964107c585c1763cee8
Roy Surjano <pxrsurja@cibertec.edu.pe>
2025-10-15 18:54:23 -0500

    adding updates

---
+++ angular-projects/projects/data-presenter/src/app/data-presenter.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DataPresenter } from '../../../user-card/src/app/data-presenter';
+
+describe('DataPresenter', () => {
+  let component: DataPresenter;
+  let fixture: ComponentFixture<DataPresenter>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DataPresenter],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DataPresenter);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular-projects/projects/user-card/src/app/user-card.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+export interface User {
+  // id name, email avatarUrl
+  id?: number;
+  name: string;
+  email: string;
+  avatarUrl?: string;
+}
+
+@Component({
+  selector: 'app-user-card',
+  imports: [CommonModule],
+  templateUrl: './user-card.html',
+  styleUrl: './user-card.css',
+})
+export class UserCard {
+  // puede contener el contrato/interfaz User o un valor nulo
+  @Input() user: User | null = null;
+
+  // flag me ayudara a validar si este card ha sido seleccionado
+  @Input() isSelected = false;
+  // este atributo va a contener un emisor de eventos
+  @Output() deleteRequest = new EventEmitter<any>();
+
+  onDeleteClick() {
+    // const isTrue = true;
+    // const name = "William Wallace"
+    console.log("clickeado en el boton eliminar")
+    // aqui tambien puedo emitir valores complejos, como objetos, arreglos
+    // aqui entonces emito el id unico del usuario
+    this.deleteRequest.emit(this.user?.id);
+  }
+
+}
